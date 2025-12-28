2cf1f0bad33cc2ad2bf7b051217b44095fa57ffa
西瓜蛋糕 <suikacake@qq.com>
2025-12-28 16:44:38 +0800

    图书馆用脚本

---
+++ 图书馆用脚本.py
@@ -0,0 +1,2 @@
+import requests
+requests.get('http://121.248.150.37:801/eportal/portal/login?callback=dr1003&login_method=1&user_account=%2C0%2Cw[你的学号]&user_password=[你的密码你&wlan_user_ipv6=&wlan_user_mac=[设备mac,此项似乎可选]&wlan_ac_ip=&wlan_ac_name=&jsVersion=4.2.2&terminal_type=1&lang=zh-cn&v=3467&lang=zh')
